048e251e16f4066a2ae11be8405b84fe89417c7d
Rafya Jamil <rafyajamil@gmail.com>
2026-07-26 09:09:54 -0700

    Add build list from permutation solution

---
+++ Lists/Build_List_from_Permutation.py
@@ -0,0 +1,11 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
+
+s = Solution()
+print(s.buildArray([0,3,2,1,4]))

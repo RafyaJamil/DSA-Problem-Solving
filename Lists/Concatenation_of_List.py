8ecb2cc317c06fd5c1a96a21f0aad1ca6cd829e3
Rafya Jamil <rafyajamil@gmail.com>
2026-07-26 04:40:56 -0700

    Add Concatenation of Array Solution

---
+++ Lists/Concatenation_of_List.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums):
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[i])
+        ans1 = ans.copy()
+        ans = ans + ans1
+        return ans
+
+s = Solution();
+print(s.getConcatenation([1,2,3,5]))

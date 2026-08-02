cee76bffd28b9f97252a5eab0e1895cdde9692c6
Rafya Jamil <rafyajamil@gmail.com>
2026-08-02 16:45:42 -0700

    Find Binary Search Solution

---
+++ Binary_Search/Binary_Search.py
@@ -0,0 +1,19 @@
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while(left <= right):
+            mid = (left + right) // 2
+            if(nums[mid] == target):
+                return mid
+            elif(nums[mid] > target):
+                right = mid - 1
+            elif(nums[mid] < target):
+                left = mid + 1
+        return -1
+
+s = Solution()
+print(s.search([2,3,4,6,8],6))

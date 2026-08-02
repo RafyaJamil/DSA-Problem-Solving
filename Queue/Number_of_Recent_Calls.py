551282bd83e9d15431d1506bf587d6692ab7b2d6
Rafya Jamil <rafyajamil@gmail.com>
2026-08-01 22:38:02 -0700

    Find Number of Recent Calls Solution

---
+++ Queue/Number_of_Recent_Calls.py
@@ -0,0 +1,20 @@
+# Time Complexity: O(1)
+# Space Complexity: O(n)
+
+from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        while( self.queue[0] < t - 3000):
+            self.queue.popleft()
+        return len(self.queue)
+
+counter = RecentCounter()
+print(counter.ping(1))
+print(counter.ping(100))
+print(counter.ping(3001))
+print(counter.ping(3002))

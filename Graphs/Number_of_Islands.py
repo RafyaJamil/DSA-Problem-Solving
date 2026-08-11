bec6bfb102120ed0530693648e33a149d06c5979
Rafya Jamil <rafyajamil@gmail.com>
2026-08-10 21:18:16 -0700

    Find Number of Islands Solution

---
+++ Graphs/Number_of_Islands.py
@@ -0,0 +1,43 @@
+# Time Complexity: O(m × n)
+# Space Complexity: O(m × n)
+
+from typing import List
+from collections import deque
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        islands = 0
+
+        directions = [
+            (-1, 0),   
+            (1, 0),    
+            (0, -1),   
+            (0, 1)     
+        ]
+
+        for r in range(rows):
+            for c in range(cols):
+
+                if grid[r][c] == "1":
+                    islands += 1
+
+                    queue = deque([(r, c)])
+                    grid[r][c] = "0"
+
+                    while queue:
+                        row, col = queue.popleft()
+
+                        for dr, dc in directions:
+                            nr = row + dr
+                            nc = col + dc
+
+                            if (0 <= nr < rows and
+                                0 <= nc < cols and
+                                grid[nr][nc] == "1"):
+
+                                grid[nr][nc] = "0"
+                                queue.append((nr, nc))
+
+        return islands

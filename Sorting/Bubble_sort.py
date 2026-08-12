52f1e9818be19f15b77b7acd994a54bde6840cf2
Rafya Jamil <rafyajamil@gmail.com>
2026-08-11 21:35:33 -0700

    Bubble sort

---
+++ Sorting/Bubble_sort.py
@@ -0,0 +1,14 @@
+def bubble_sort(nums):
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+    return nums
+
+
+nums = [5, 2, 8, 1, 3]
+print(bubble_sort(nums))
